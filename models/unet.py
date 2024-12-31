ca6ec657f04fe15e7c0d6e1e61fcdbecec1590dc
zeyadcode <zeyadshapan2004@gmail.com>
2024-12-31 07:50:55 +0200

    fixed wrong device model

--- models/unet.py
+++ models/unet.py
@@ -6,8 +6,11 @@ def load_model_unet(checkpoint_path: str = None) -> torch.nn.Module:
     Uses retinaNet model
     :return: torch.Module
     """
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
     model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=1,
-                           init_features=32, pretrained=False)
+                           init_features=32, pretrained=False).to(device)
+
 
     if checkpoint_path is not None:
         model.load_state_dict(torch.load(checkpoint_path, weights_only=True))
